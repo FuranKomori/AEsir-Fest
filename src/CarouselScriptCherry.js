0f570b025dd12ec708219711fb17bb69b1d58f83
Furan <fran.araya.k@gmail.com>
2019-09-10 13:19:29 -0300

    new file:   About.html 	new file:   Cherry.html 	new file:   Main.html 	new file:   Neko.html 	new file:   Paff.html 	new file:   ROBO_Head.html 	new file:   Registration.html 	new file:   img/AESIR1.png 	new file:   img/AEsir_FEST1.jpg 	new file:   img/AEsir_FULL.png 	new file:   img/Cherry.png 	new file:   img/Cherry_Logo.png 	new file:   img/Cherry_g1.jpg 	new file:   img/CyTus.jpg 	new file:   img/CyTus2.png 	new file:   img/CyTus3.webp 	new file:   img/Neko.png 	new file:   img/Neko_Logo.png 	new file:   img/Neko_g1.png 	new file:   img/Paff.png 	new file:   img/Paff_Logo.png 	new file:   img/ROBO_Head.png 	new file:   img/ROBO_Head_Logo.png 	new file:   img/ROBO_Head_g1.png 	new file:   img/ROBO_Head_g2.png 	new file:   img/ROBO_Head_g3.jpg 	new file:   img/neko_g2.png 	new file:   img/neko_g3.png 	new file:   img/neko_g4.png 	new file:   img/paff_g1.png 	new file:   img/paff_g2.png 	new file:   img/paff_g3.png 	new file:   music/Blow_My_Mind.mp3 	new file:   music/Fight_Another_Day.mp3 	new file:   music/Pure_Powerstomper.mp3 	new file:   music/RETRIEVE.mp3 	new file:   music/silence.mp3 	new file:   src/Carousel/.gitignore 	new file:   src/Carousel/.npmignore 	new file:   src/Carousel/LICENSE 	new file:   src/Carousel/README.md 	new file:   src/Carousel/bower.json 	new file:   src/Carousel/dist/jR3DCarousel.min.js 	new file:   src/Carousel/index.html 	new file:   src/Carousel/package.json 	new file:   src/Carousel/src/jR3DCarousel.js 	new file:   src/CarouselScript.js 	new file:   src/CarouselScriptCherry.js 	new file:   src/CarouselScriptNeko.js 	new file:   src/CarouselScriptPaff.js 	new file:   src/CarouselScriptROBO.js 	new file:   src/HeaderSticky.js 	new file:   src/jR3DCarousel.js 	new file:   styles/Customization.css

---
+++ src/CarouselScriptCherry.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	var slideImages = [ {src: 'img/cherry_logo.png'},
+						{src: 'img/cherry_g1.jpg'} ]
+	//var jR3DCarousel;
+	
+	jR3DCarousel = $('.jR3DCarouselGallery').jR3DCarousel({
+		width: 480, 		/* largest allowed width */
+		height: 320, 		/* largest allowed height */
+		slides: slideImages /* array of images source */
+	});
+	
+	var carouselCustomeTemplateProps =  {
+	 		  width: 470, 				/* largest allowed width */
+			  height: 272, 				/* largest allowed height */
+			  slideLayout : 'fill',     /* "contain" (fit according to aspect ratio), "fill" (stretches object to fill) and "cover" (overflows box but maintains ratio) */
+			  animation: 'slide3D', 	/* slide | scroll | fade | zoomInSlide | zoomInScroll */
+			  animationCurve: 'ease',
+			  animationDuration: 1900,
+			  animationInterval: 2000,
+			  slideClass: 'jR3DCarouselCustomSlide',
+			  autoplay: false,
+			  controls: true,			/* control buttons */
+			  navigation: ''			/* circles | squares | '' */,
+			  perspective: 2200,
+			  rotationDirection: 'ltr',
+			  onSlideShow: slideShownCallback
+				  
+		}
+	function slideShownCallback($slide){
+		console.log("Slide shown: ", $slide.find('img').attr('src'))
+	}
+
+	jR3DCarouselCustomeTemplate = $('.jR3DCarouselGalleryCustomeTemplate').jR3DCarousel(carouselCustomeTemplateProps);
+
+  })
